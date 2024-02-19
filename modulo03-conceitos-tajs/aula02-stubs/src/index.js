50d1bdab7bc811d6745b9c96cfaf6f7a071d7bcd
Anderson Prado <andersonpvsilva@gmail.com>
2024-02-18 21:46:26 -0300

    feat: implemented stubs

---
+++ modulo03-conceitos-tajs/aula02-stubs/src/index.js
@@ -0,0 +1,16 @@
+import Service from "./service.js"
+
+const data = {
+    username: `clebinho-${Date.now()}`,
+    password: 'senha'
+}
+
+const service = new Service({
+    filename: './users.ndjson'
+})
+
+await service.create(data)
+
+const users = await service.read()
+
+console.log('users', users) 
